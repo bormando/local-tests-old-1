c5503bfd3ba5ed801976fba899114962d00cd7ab
Dmitrii Bormotov <squier7@gmail.com>
2023-01-14 14:08:13 +0300

    Add Profile page

---
+++ cypress/pages/app/profile.page.js
@@ -0,0 +1,9 @@
+import AppPage from './app.page'
+
+class ProfilePage extends AppPage {
+  open() {
+    return super.open(`/profile/${Cypress.env('userId')}`)
+  }
+}
+
+export default new ProfilePage()
